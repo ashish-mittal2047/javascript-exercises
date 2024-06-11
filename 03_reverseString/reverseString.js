396223e8af4c1862f52d1984b8daf0b98afa54da
ashish.mittal2047 <68066532+ashish-mittal2047@users.noreply.github.com>
2024-06-12 00:36:23 +0530

    Write reverseString function First test case passed, 3 test cases skipped

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function (strToReverse) {
+  if (strToReverse === '')
+    return '';
+  else {
+    arrayFormOfStr = strToReverse.split("");
+    arrayFormOfStr.reverse();
+    let reversedStr = arrayFormOfStr.join("");
+    return reversedStr;
+  }
 };
 
 // Do not edit below this line
