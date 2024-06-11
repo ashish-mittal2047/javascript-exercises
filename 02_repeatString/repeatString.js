8e45026816c81b4c09b018dcdf2682995a3cbaee
ashish.mittal2047 <68066532+ashish-mittal2047@users.noreply.github.com>
2024-06-12 00:23:58 +0530

    Modify repeatString as per README file

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
-
+const repeatString = function (inputStr, repeatCount) {
+  if (repeatCount === 0) {
+    return '';
+  }
+  else if (repeatCount < 0) {
+    return 'ERROR';
+  }
+  else {
+    let outputStr = '';
+    for (let i = 0; i < repeatCount; i++) {
+      outputStr += inputStr;
+    }
+    return outputStr;
+  }
 };
 
 // Do not edit below this line
