8f3db709266da43e04ffc804b0c1a5e474830a74
ashish.mittal2047 <68066532+ashish-mittal2047@users.noreply.github.com>
2024-06-12 01:23:44 +0530

    Write sumAll function to get sum between args 1 test case passed 1 issue occured i.e typeof x returns a value of type string. For e.g typeof 1 === 'number'. It was fixed with the help of debugger.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function (startingNum, endingNum) {
+  if ((typeof startingNum) !== 'number' || (typeof endingNum) !== 'number') {
+    return "ERROR";
+  }
+  if (startingNum < 0 || endingNum < 0) {
+    return "ERROR";
+  }
+  let finalSum = 0;
+  if (startingNum > endingNum) {
+    let tempVar = startingNum;
+    startingNum = endingNum;
+    endingNum = tempVar;
+  }
+  for (let i = startingNum; i <= endingNum; i++) {
+    finalSum += i;
+  }
+  return finalSum;
 };
 
 // Do not edit below this line
