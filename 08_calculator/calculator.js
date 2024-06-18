17b7dd150724964f0b0ee83c73ba2b5c3a214198
ashish.mittal2047 <68066532+ashish-mittal2047@users.noreply.github.com>
2024-06-18 21:40:36 +0530

    Solve assignment 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,37 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (array) {
+  return array.reduce((total, curElement) => {
+    return total + curElement;
+  }, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (array) {
+  let product = 1;
+  for (let i = 0; i < array.length; i++)
+    product *= array[i];
+  return product;
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function (a) {
+  let fact = 1;
+  for (let i = 1; i <= a; i++) {
+    fact *= i;
+  }
+  return fact;
 };
+
+console.log(sum([]));
 
 // Do not edit below this line
 module.exports = {
