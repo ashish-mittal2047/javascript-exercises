d051aea5b0416ab37601d3a00a3636350061ed42
ashish.mittal2047 <68066532+ashish-mittal2047@users.noreply.github.com>
2024-06-18 22:39:25 +0530

    Solve assignment 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,24 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+  people.sort((person1, person2) => {
+    let age1;
+    if ('yearOfDeath' in person1) {
+      age1 = person1.yearOfDeath - person1.yearOfBirth;
+    }
+    else {
+      let curYear = new Date().getFullYear();
+      age1 = curYear - person1.yearOfBirth;
+    }
+    let age2;
+    if ('yearOfDeath' in person2) {
+      age2 = person2.yearOfDeath - person2.yearOfBirth;
+    }
+    else {
+      let curYear = new Date().getFullYear();
+      age2 = curYear - person2.yearOfBirth;
+    }
+    return age1 - age2;
+  });
+  return people.pop();
 };
 
 // Do not edit below this line
