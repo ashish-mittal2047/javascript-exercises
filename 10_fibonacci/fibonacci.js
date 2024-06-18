a3f5b3d260cd73d906e830460e7500ed061a285a
ashish.mittal2047 <68066532+ashish-mittal2047@users.noreply.github.com>
2024-06-18 22:26:11 +0530

    solve assignment 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
-
+const fibonacci = function (n) {
+  n = Number(n);
+  if (isNaN(n) || n < 0)
+    return "OOPS";
+  if (n === 0)
+    return 0;
+  else if (n === 1)
+    return 1;
+  else {
+    let a = 1;
+    let b = 0;
+    for (let i = 2; i <= n; i++) {
+      ans = a + b;
+      b = a;
+      a = ans;
+    }
+    return ans;
+  }
 };
 
 // Do not edit below this line
