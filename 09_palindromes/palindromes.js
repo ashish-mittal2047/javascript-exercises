d373e3b2a36e7320ea598c31b7abce0ea7f204b2
ashish.mittal2047 <68066532+ashish-mittal2047@users.noreply.github.com>
2024-06-18 22:15:26 +0530

    Solve assignment 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
+const alphanumeric = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
 
+const palindromes = function (str) {
+  str = str.toLowerCase().split('').filter((char) => {
+    return alphanumeric.includes(char);
+  })
+    .join('');
+  if (str === str.split('').toReversed().join(''))
+    return true;
+  else return false;
 };
 
 // Do not edit below this line
